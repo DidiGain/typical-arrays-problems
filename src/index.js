bb5caaf7aaedb5b45abca09a6f7ab842fe150f1a
Didi <dignara@yandex.ru>
2021-02-10 14:10:15 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+  if (arguments.length === 0) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (arguments.length === 0) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
+};
+
+const sum = (array) => {
+  let count = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    count += array[i];
+  }
+  return count;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (arguments.length === 0) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  return sum(array) / array.length;
+};
